977840e7eecb79a6434dc5f53e61bedfa81b6be2
AbdullahRFA <shakibrybmn@gmail.com>
2025-07-16 23:44:14 +0600

    added resources

--- DIP/DIP_Lab_406/How to show an image/method_2.py
+++ DIP/DIP_Lab_406/How to show an image/method_2.py
@@ -3,5 +3,6 @@ import matplotlib.image as mpimg
 
 img = mpimg.imread('/Users/abdullahnazmus-sakib/Desktop/995F9C93-2B51-4DF9-A785-FB27757BCA5D.png')
 plt.imshow(img)
+plt.title("Class Routine")
 plt.axis('off')
 plt.show()
